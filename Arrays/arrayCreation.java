c7f402c1b26653b9398ecd3f0c6ea6f764d09da8
Shreya-Wani <shreyawani2006@gmail.com>
2025-12-30 00:31:11 +0530

    Array-creation

---
+++ Arrays/arrayCreation.java
@@ -0,0 +1,9 @@
+public class arrayCreation{
+    public static void main(String args[]) {
+        int marks[] = new int[50];
+
+        int numbers[] = {1, 2, 3};
+
+        String fruits[] = {"apple", "mango"};
+    }
+}
